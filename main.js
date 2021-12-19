9442dd73fb6805e6fb438cb46c580332ab90a916
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2021-12-19 05:28:03 +0100

    Refactor code
    Set constants, helper functions and main functions.
    
    Developed a proper event handler.

--- main.js
+++ main.js
@@ -1,41 +1,36 @@
 const axios = require("axios");
-const fs = require("fs");
 
-let cookie = "e535f2184f715ef1b7b37fcf602c6c23";
-const out = "./outs.json"
+// Constants:
+const COOKIE = "e535f2184f715ef1b7b37fcf602c6c23";
+const CURRENT_YEAR = new Date(Date.now()).getFullYear();
+const CURRENT_MONTH = new Date(Date.now()).getMonth();
 
-async function get_data(year=2021, month=11) {
+// Helper functions:
+function textify(event) {
+	return `Name: ${event.name}\nDescription: ${event.description}\nCourse name: ${event.course_name}\nCourse category: ${event.course_category}\nDate: ${event.date}\nURL: ${event.url}`
+}
+
+// 1. Content retrieval:
+async function get_data(year=CURRENT_YEAR, month=CURRENT_MONTH) {
 	return (await axios.post(
 		"https://moovi.uvigo.gal/lib/ajax/service.php?sesskey=wQ2GngY4A6&info=core_calendar_get_calendar_monthly_view",
 		[{"index":0,"methodname":"core_calendar_get_calendar_monthly_view","args":{"year":year,"month":month,"courseid":1,"categoryid":0,"includenavigation":false,"mini":true,"day":1}}],
-		{headers: {Cookie: `MoodleSession=${cookie}`}}
+		{headers: {Cookie: `MoodleSession=${COOKIE}`}}
 	)).data[0]
 }
 
-
-function get_date(seconds) {
-	return new Date(seconds * 1000);
-}
-
-
+// 2. Content handling:
 get_data().then(function(response) {
-	let weeks = response.data.weeks;
-
-	// let days = weeks.map(function (week, index) {return week.days.map(function (day, index) {return day.daytitle})});
-
-	let days = weeks.map(function(week) {return week.days});
-	console.log(days);
-
-	console.log("\n\n\n\n\n\n\n");
-
-	// let days = weeks.map(function(week) {return week.days.map(function(day) {return day.daytitle})}).flat();
-	// console.log(days);
-
-	console.log("\n\n\n\n\n\n\n");
-
-	let events = days.map(function(day) {return day.events.map(function(event) {return get_date(event.timestart)})}).flat();
-	console.log(events);
-
-	let courses = weeks.map(function(week) {return week.days.map(function(day) {return day.events.map(function(event) {return event.course[0].timestart})})}).flat();
-	console.log(courses);
+	let weeks = response.data.weeks.map(function (week) {return week.days});
+	let days = weeks.flat()
+	let daytitles = days.map(function (day) {return day.daytitle});
+	let events = days.map(function (day) {return day.events}).flat().map(function (event) {return {
+		name: event.name,
+		description: event.description.match(/(?<=[>])[\s\S]*?(?=[<])/g) == null ? [] : event.description.match(/(?<=[>])[\s\S]*?(?=[<])/g).filter(function (content) {return content.length > 0 ? content : null}).map(function (content) {return content.trim()}).join(' '),
+		course_name: event.course.fullnamedisplay,
+		course_category: event.course.coursecategory,
+		date: new Date(event.timestart * 1000),
+		url: event.url
+	}})
+	console.log(textify(events[0]));
 })
