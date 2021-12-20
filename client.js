30721bdaf368fed37642d630170d5526dd227181
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2021-12-20 23:00:17 +0100

    Convert spaces to tabs

--- client.js
+++ client.js
@@ -11,13 +11,12 @@ const SUPERUSERS = process.env.SUPERUSERS.split(',');
 const SESSION_FILE_PATH = './session.json';
 let client, sessionData;
 
-
 // Helper functions:
 
 /**Function that adds a custom timestamp to the logged message (debugging purposes).*/
 function logger(message, content = '') {
-    let now = new Date(Date.now());
-    console.log(`[${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}] -> ${content} ${JSON.stringify(message)}\n`);
+	let now = new Date(Date.now());
+	console.log(`[${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}] -> ${content} ${JSON.stringify(message)}\n`);
 }
 
 
@@ -26,123 +25,124 @@ function logger(message, content = '') {
 /**Function that contains Whatsapp bot's mainloop.*/
 async function init() {
 
-    // Session authentication:
+	// Session authentication:
 
-    if (fs.existsSync(SESSION_FILE_PATH)) {
-        sessionData = require(SESSION_FILE_PATH);
-    }
+	if (fs.existsSync(SESSION_FILE_PATH)) {
+		sessionData = require(SESSION_FILE_PATH);
+	}
 
-    client = new Client({
-        session: sessionData
-    });
+	client = new Client({
+		session: sessionData
+	});
 
-    client.on('authenticated', (session) => {
-        sessionData = session;
-        fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
-            if (err) {
-                console.error(err);
-            }
-        });
-    });
+	client.on('authenticated', (session) => {
+		sessionData = session;
+		fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
+			if (err) {
+				console.error(err);
+			}
+		});
+	});
 
-    client.on('qr', qr => {
-        qrcode.generate(qr, {
-            small: true
-        });
-    });
+	client.on('qr', qr => {
+		qrcode.generate(qr, {
+			small: true
+		});
+	});
 
-    client.on('ready', () => {
-        logger('Client is ready');
-    });
+	client.on('ready', () => {
+		logger('Client is ready');
+	});
 
 
-    // Bot commands:
+	// Bot commands:
 
-    client.on('message_create', message => {
-        if (message.type == 'list_response') {
-            logger(message, 'List response detected.');
-            listReplyHandler(message);
-        } else if (message.type == 'buttons_response') {
-            logger(message, 'Button response detected.');
-            buttonReplyHandler(message);
-        } else if (SUPERUSERS.includes(message.from)) {
-            switch (message.body) {
-                case 'getid':
-                    logger(message, 'ID requested.');
-                    message.reply(message.to);
-                    break;
-                case 'getevents':
-                    logger(message, 'Live events requested.');
-                    sendEvents(message);
-                    break;
-                case 'getcachedevents':
-                    logger(message, 'Cached events requested.');
-                    cachedEvents(message);
-                    break;
-                case 'geteventlist':
-                    logger(message, 'Event list requested.');
-                    sendEventList(message);
-                    break;
-                default:
-                    logger(message, 'No matches for the requested message.');
-                    break;
-            }
-        } else {
-            logger(message, 'Non-commanded message.');
-        }
-    });
+	client.on('message_create', message => {
+		if (message.type == 'list_response') {
+			logger(message, 'List response detected.');
+			listReplyHandler(message);
+		} else if (message.type == 'buttons_response') {
+			logger(message, 'Button response detected.');
+			buttonReplyHandler(message);
+		} else if (SUPERUSERS.includes(message.from)) {
+			switch (message.body) {
+				case 'getid':
+					logger(message, 'ID requested.');
+					message.reply(message.to);
+					break;
+				case 'getevents':
+					logger(message, 'Live events requested.');
+					sendEvents(message);
+					break;
+				case 'getcachedevents':
+					logger(message, 'Cached events requested.');
+					cachedEvents(message);
+					break;
+				case 'geteventlist':
+					logger(message, 'Event list requested.');
+					sendEventList(message);
+					break;
+				default:
+					logger(message, 'No matches for the requested message.');
+					break;
+			}
+		} else {
+			logger(message, 'Non-commanded message.');
+		}
+	});
 
-    client.initialize();
+	client.initialize();
 }
 
 /**Function that fetches and sends event data from the local database instead of the remote one.*/
 async function cachedEvents(message) {
-    let events = require(moovi.DATABASE_FILE);
-    Object.keys(events).map(function (event) {client.sendMessage(message.to, moovi.eventStringify(events[event]))});
+	let events = require(moovi.DATABASE_FILE);
+	Object.keys(events).map(function (event) {client.sendMessage(message.to, moovi.eventStringify(events[event]))});
 }
 
 /**Function that sends each formatted event to the message sender's chat.*/
 async function sendEvents(message) { // DEPRECATED
-    (await moovi.getEvents(await moovi.getCalendarData())).map(function (event) {
-        client.sendMessage(message.to, moovi.eventStringify(event));
-    });
+	(await moovi.getEvents(await moovi.getCalendarData())).map(function (event) {
+		client.sendMessage(message.to, moovi.eventStringify(event));
+	});
 }
 
 /**Function that sends a formatted event list to the message sender's chat.*/
 async function sendEventList(message) {
-    let events = require(moovi.DATABASE_FILE);
-    let identifiers = Object.keys(events);
+	let events = require(moovi.DATABASE_FILE);
+	let identifiers = Object.keys(events);
 
-    let rows = [];
-    let sections = [{title: 'Eventos:', rows: rows}];
+	let rows = [];
+	let sections = [{title: 'Eventos:', rows: rows}];
 
-    identifiers.forEach(function (identifier) {
-        sections[0].rows.push({
-            id: identifier,
-            title: events[identifier].name,
-            description: events[identifier].description != '' ? events[identifier].description.length <= 39 ? events[identifier].description : events[identifier].description.slice(0, 39) + '...' : 'No existe descripción para este evento.'
-        });
-    });
+	identifiers.forEach(function (identifier) {
+		sections[0].rows.push({
+			id: identifier,
+			title: events[identifier].name,
+			description: events[identifier].description != '' ? events[identifier].description.length <= 39 ? events[identifier].description : events[identifier].description.slice(0, 39) + '...' : 'No existe descripción para este evento.'
+		});
+	});
 
-    client.sendMessage(message.to, new List('_Pulsa el botón de la parte inferior para ver todos los eventos disponibles._', 'Ver eventos', sections, '*Próximos eventos:*', ''));
+	client.sendMessage(message.to, new List('_Pulsa el botón de la parte inferior para ver todos los eventos disponibles._', 'Ver eventos', sections, '*Próximos eventos:*', ''));
 }
 
 /**Function that serves as handler for list-specific replies.*/
 async function listReplyHandler(message) {
-    let event = require(moovi.DATABASE_FILE)[message.selectedRowId];
-    let button = new Buttons(
-        moovi.eventStringify(event),
-        [{id: `${event.date}`, body:'Tiempo restante'}],
-        '',
-        ''
-    );
-    message.reply(button);
+	let event = require(moovi.DATABASE_FILE)[message.selectedRowId];
+	let button = new Buttons(
+		`\n${moovi.eventStringify(event)}`,
+		[{id: `${event.date}`, body: 'Ver tiempo restante'}],
+		`Evento: ${event.name.toUpperCase()}`,
+		`\nVisita ${event.url} para más información acerca del evento.`
+	);
+	// message.reply(button);
+	client.sendMessage(message.from, button)
 }
 
 async function buttonReplyHandler(message) {
-    let difference = moovi.dateDifference(new Date(Date.now()), new Date(parseInt(message.selectedButtonId)));
-    message.reply(`Tiempo restante: ${difference.days} día(s), ${difference.hours} hora(s), ${difference.minutes} minuto(s), ${difference.seconds} segundo(s).`);
-    client.sendMessage(message.from, `Tiempo restante: ${difference.days} día(s), ${difference.hours} hora(s), ${difference.minutes} minuto(s), ${difference.seconds} segundo(s).`);
+	let difference = moovi.dateDifference(new Date(Date.now()), new Date(parseInt(message.selectedButtonId)));
+	message.reply(`Tiempo restante: ${difference.days} día(s), ${difference.hours} hora(s), ${difference.minutes} minuto(s), ${difference.seconds} segundo(s).`);
+	client.sendMessage(message.from, `Tiempo restante: ${difference.days} día(s), ${difference.hours} hora(s), ${difference.minutes} minuto(s), ${difference.seconds} segundo(s).`);
 }
 
 
