900fe30c17a540aafcd1bf992047dc342a6ba30a
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2021-12-20 18:38:10 +0100

    Refactor code
    Removed `getEvents`-styled functions and replaced them with the `getUpcomingEvents`, which are now the standard.
    
    Relocated some code to provide with a clearer workspace.
    
    Added 5129 missing semicolons.

--- mooviFunctions.js
+++ mooviFunctions.js
@@ -6,41 +6,7 @@ const url = require('url'); // Encoded data tool for URL-params.
 
 require('dotenv').config();
 
-let DATABASE = './events.json';
-
-
-// Format functions:
-
-/**Function that returns a formatted string with event data.*/
-function eventStringify(event) { // TODO: add remaining time parameter.
-
-	return ('' + // FIXME: Maybe use a filter method to simplify this expression?
-		`${event.course_category == '' ? '': '\`\`\`Curso:\`\`\`' + event.course_category + '\n\n'}` +
-		`${event.course_category == '' ? '': '\`\`\`Módulo:\`\`\`' + event.course_name + '\n\n'}` +
-		`\`\`\`Evento:\`\`\` *${event.name.toUpperCase()}*\n\n` +
-		`\`\`\`Fecha límite:\`\`\` ${new Date(event.date).toLocaleString('en-GB', { timeZone: 'UTC' })}\n\n` +
-		`${event.location == '' ? '' : '\`\`\`Localización:\`\`\`' + event.location + '\n\n'}` +
-		`${event.description == '' ? '' : '\`\`\`Descripción:\`\`\`' + event.description + '\n\n'}` +
-		`\`\`\`URL:\`\`\` ${event.url}`
-	);
-}
-
-/**Function that returns an object containing the time difference between two EPOCH dates.*/
-function format_difference(date1, date2) {
-
-	let diff = Math.abs(date1 - date2); // Returns the ms difference.
-	days = diff / 86400000;
-	hours = days % 1 * 24;
-	minutes = hours % 1 * 60;
-	seconds = minutes % 1 * 60;
-
-	return {
-		days: Math.floor(days),
-		hours: Math.floor(hours),
-		minutes: Math.floor(minutes),
-		seconds: Math.floor(seconds)
-	}
-}
+let DATABASE_FILE = './events.json';
 
 
 // WebService Authentication:
@@ -49,7 +15,7 @@ function format_difference(date1, date2) {
  * Allows automatic ws_token variable definition (with no additional .env parameters).*/
 async function initToken() {
 
-	ws_token = await getWsToken(process.env.MOOVI_USERNAME, process.env.MOOVI_PASSWORD)
+	ws_token = await getWsToken(process.env.MOOVI_USERNAME, process.env.MOOVI_PASSWORD);
 	return true;
 }
 
@@ -78,17 +44,17 @@ async function getWsToken(username, password) {
 async function wsRequest(ws_function, req_params) {
 
 	if (typeof ws_token === 'undefined') {
-		await initToken()
+		await initToken();
 	}
 	return (await axios.post('https://moovi.uvigo.gal/webservice/rest/server.php', new url.URLSearchParams({
 		...req_params,
 		wsfunction: ws_function,
-		wstoken: ws_token,
+		wstoken: ws_token
 	}).toString(), {
 		params: {
-			moodlewsrestformat: 'json',
+			moodlewsrestformat: 'json'
 		}
-	})).data
+	})).data;
 }
 
 /**Function that requests Moodle's calendar data for the next two months.*/
@@ -96,42 +62,19 @@ async function getCalendarData(c_year = new Date(Date.now()).getFullYear(), c_mo
 
 	let current_month = (await wsRequest('core_calendar_get_calendar_monthly_view', {
 		year: c_year,
-		month: c_month,
-	})).weeks
+		month: c_month
+	})).weeks;
 
 	let next_month = (await wsRequest('core_calendar_get_calendar_monthly_view', {
 		year: c_month == 12 ? c_year + 1 : c_year,
 		month: c_month == 12 ? 1 : c_month + 1
-	})).weeks
+	})).weeks;
 
 	return current_month.concat(next_month);
 }
 
-/**Function that processes previously fetched calendar data.*/
-async function getEvents(calendar_data) {
-	return calendar_data.map(function (week) {
-		return week.days
-	}).flat().map(function (day) {
-		return day.events
-	}).flat().map(function (event) {
-		return {
-			name: event.name,
-			description: (event.description.match(/(?<=[>])[\s\S]*?(?=[<])/g) == null ? [] : event.description.match(/(?<=[>])[\s\S]*?(?=[<])/g).filter(function (content) {
-				return content.length > 0 ? content : null
-			}).map(function (content) {
-				return content.replace(/[\r\n]+/g, " ").trim()
-			})).join(' ').replace(/(\s\.)+/g, "."),
-			course_name: "course" in event ? event.course.fullnamedisplay : '',
-			course_category: "course" in event ? event.course.coursecategory : '',
-			date: (event.timestart + 3600) * 1000, // Hourly delay compensation.
-			location: event.location,
-			url: event.url,
-		}
-	})
-}
-
 /**Function that processes previously fetched calendar data but only for upcoming events.*/
-async function getUpcomingEvents(calendar_data) {
+async function getEvents(calendar_data) {
 	return calendar_data.map(function (week) {
 		return week.days
 	}).flat().map(function (day) {
@@ -151,8 +94,8 @@ async function getUpcomingEvents(calendar_data) {
 			url: event.url
 		}
 	}).filter(function (event) {
-		return event.date >= new Date(Date.now()) ? event : null
-	})
+		return event.date >= new Date(Date.now()) ? event : null;
+	});
 }
 
 /**Returns a unique identifier for each event.*/
@@ -162,11 +105,11 @@ function getId(event) {
 
 /**Updates a JSON database that contains every event.*/
 async function updateEvents() {
+	let identifier;
 	let data = await getEvents(await getCalendarData());
 	let data_entries = data.map(function (event) {return getId(event)});
-	let current_entries = require(DATABASE);
+	let current_entries = require(DATABASE_FILE);
 	let old_entries = {...current_entries};
-	let identifier;
 
 	// Include added entries:
 	data.forEach(function (entry) {
@@ -185,29 +128,63 @@ async function updateEvents() {
 
 	// Write output:
 	if (JSON.stringify(current_entries) !== JSON.stringify(old_entries)) {
-		console.log("Modified entries, saving data.")
-		fs.writeFile(DATABASE, JSON.stringify(current_entries), (err) => {
-        	if (err) {
-            	console.error(err);
-    	    }
-	    });
+		console.log("Modified entries, saving data.");
+		fs.writeFile(DATABASE_FILE, JSON.stringify(current_entries), (err) => {if (error) {console.error(error)}});
 	} else {
 		console.log("No modified entries, keeping original data.");
 	}
 }
 
-updateEvents();
+
+// Format functions:
+
+/**Function that returns a formatted string with event data.*/
+function eventStringify(event) { // TODO: add remaining time parameter.
+
+	return ('' + // FIXME: Maybe use a filter method to simplify this expression?
+		`${event.course_category == '' ? '': '\`\`\`Curso:\`\`\`' + event.course_category + '\n\n'}` +
+		`${event.course_category == '' ? '': '\`\`\`Módulo:\`\`\`' + event.course_name + '\n\n'}` +
+		`\`\`\`Evento:\`\`\` *${event.name.toUpperCase()}*\n\n` +
+		`\`\`\`Fecha límite:\`\`\` ${new Date(event.date).toLocaleString('en-GB', { timeZone: 'UTC' })}\n\n` +
+		`${event.location == '' ? '' : '\`\`\`Localización:\`\`\`' + event.location + '\n\n'}` +
+		`${event.description == '' ? '' : '\`\`\`Descripción:\`\`\`' + event.description + '\n\n'}` +
+		`\`\`\`URL:\`\`\` ${event.url}`
+	);
+}
+
+/**Function that returns an object containing the time difference between two EPOCH dates.*/
+function dateDifference(date1, date2) {
+
+	let diff = Math.abs(date1 - date2); // Returns the ms difference.
+	days = diff / 86400000;
+	hours = days % 1 * 24;
+	minutes = hours % 1 * 60;
+	seconds = minutes % 1 * 60;
+
+	return {
+		days: Math.floor(days),
+		hours: Math.floor(hours),
+		minutes: Math.floor(minutes),
+		seconds: Math.floor(seconds)
+	};
+}
+
 
 // Module export:
 
 module.exports = {
-	eventStringify,
-	format_difference,
-	getWsToken,
+	DATABASE_FILE,
+
 	initToken,
 	wsRequest,
-	getCalendarData,
+
+	getId,
 	getEvents,
-	getUpcomingEvents,
-	updateEvents
+	getWsToken,
+	getCalendarData,
+
+	updateEvents,
+
+	eventStringify,
+	dateDifference
 }
