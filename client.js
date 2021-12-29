4af958ca5ea600b1d30a270f8fe2d2d343b59cb1
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2021-12-29 03:10:28 +0100

    Add user interactions
    Added user interactions for the bot. Provided with "authentication" methods for both addition and removal of a user from a notification list, which is degree and year-specific.
    
    Updated superuser verification for specific commands and modified command syntax recognition.
    
    Added error handling for each one of the newly implemented methods.
    
    Updated sender methods so that they feature multiple database fetching and message sending proceses.

--- client.js
+++ client.js
@@ -1,23 +1,27 @@
+// Remote modules:
 const fs = require('fs')
 const qrcode = require('qrcode-terminal')
 const { Client, List, Buttons } = require('whatsapp-web.js')
-const formatter = require('./formatter')
+
+// Local modules:
 const users = require('./users')
-
-require('dotenv').config()
-const SUPERUSERS = process.env.SUPERUSERS.split(',')
+const event_fetcher = require('./event_fetcher')
 const { logger } = require('./aux')
+const { SUPERUSERS } = require('./auth')
 
+// Constants and variables:
 const SESSION = './session.json'
 let client, sessionData, chat, content
 
 
+/**Bot's authentication and mainloop.*/
 async function session_initialize() {
 
 	// Session:
 
 	fs.existsSync(SESSION) ? sessionData = require(SESSION) : null
 	client = new Client({ session: sessionData })
+
 
 	client.on('authenticated', session => {
 		sessionData = session
@@ -33,100 +37,166 @@ async function session_initialize() {
 
 	client.on('message_create', async message => {
 		chat = await message.getChat()
+		content = message.body.split(' ')
+		superuser = SUPERUSERS.includes(message.from)
 
 		if (message.type == 'list_response') {
-			logger(message, 'List response detected.')
+			logger('List response detected.', message)
 			listReplyHandler(message)
 
 		} else if (message.type == 'buttons_response') {
-			logger(message, 'Button response detected.')
+			logger('Button response detected.', message)
 			buttonReplyHandler(message)
 
-		} else if (SUPERUSERS.includes(message.from)) {
-			content = message.body.split(' ')
+		} else {
 			switch (content[0]) {
 				case 'getid':
 					chat.sendStateTyping()
-					logger(message, 'ID requested.')
-					client.sendMessage(message.from, `El ID del chat solicitado es: ${message.to}`)
-					chat.clearState()
-					break
-
-				case 'geteventlist':
-					chat.sendStateTyping()
-					logger(message, 'Event list requested.')
-					sendEventList(message)
+					logger('ID requested.', message)
+					superuser ? client.sendMessage(message.from, `El ID del chat solicitado es: ${message.to}`) : null
 					chat.clearState()
 					break
 
 				case 'getsession':
 					chat.sendStateTyping()
-					logger(message, 'Session JSON requested.')
-					client.sendMessage(message.from, 'Con el fin de evitar problemas de seguridad, copie el código de sesión del mensaje siguiente y proceda a eliminarlo.')
-					client.sendMessage(message.from, `${JSON.stringify(require(SESSION))}`)
+					logger('Session JSON requested.', message)
+					if (superuser) {
+						client.sendMessage(message.from, 'Con el fin de evitar problemas de seguridad, copie el código de sesión del mensaje siguiente y proceda a eliminarlo.')
+						client.sendMessage(message.from, `${JSON.stringify(require(SESSION))}`)
+					}
+					chat.clearState()
+					break
+
+				case 'adddegree':
+					chat.sendStateTyping()
+					logger('Degree addition requested.', message)
+					if (superuser) {
+						switch (users.addDegree(content[1])) {
+							case 0:
+								client.sendMessage(message.from, `El grado en ${content[1]} ha sido añadido correctamente.`)
+								break
+							case 1:
+								client.sendMessage(message.from, `El grado en ${content[1]} ya existe.`)
+								break
+						}
+					}
 					chat.clearState()
 					break
 
 				case 'addme':
 					chat.sendStateTyping()
-					users.addUser(message.from, content[1], content[2])
+					logger('User addition requested.', message)
+					switch (users.addUser(message.from, content[1], content[2])) {
+						case 0:
+							client.sendMessage(message.from, `Ha sido añadido correctamente a la lista de notificaciones de ${content[2]}º del grado en ${content[1]}.`)
+							break
+						case 1:
+							client.sendMessage(message.from, `El grado en ${content[1]} no existe. Contacte con un administrador para resolver la incidencia.`)
+							break
+						case 2:
+							client.sendMessage(message.from, `El curso ${content[2]}º del grado en ${content[1]} no existe. Contacte con un administrador para resolver la incidencia.`)
+							break
+						case 3:
+							client.sendMessage(message.from, `Usted ya se encuentra registrado en ${content[2]}1 del grado en ${content[1]}.`)
+							break
+					}
 					chat.clearState()
 					break
 
 				case 'removeme':
 					chat.sendStateTyping()
-					users.removeUser(message.from)
+					logger('User removal requested.', message)
+					switch (users.removeUser(message.from, content[1], content[2])) {
+						case 0:
+							client.sendMessage(message.from, `Ha sido retirado correctamente de la lista de notificaciones de ${content[2]}º del grado en ${content[1]}.`)
+							break
+						case 1:
+							client.sendMessage(message.from, `Usted no se encuentra matriculado en ${content[2]}º del grado de ${content[1]}.`)
+							break
+						case 2:
+							client.sendMessage(message.from, `Usted no se encuentra matriculado en el grado de ${content[1]}.`)
+							break
+						case 3:
+							client.sendMessage(message.from, `El curso ${content[2]}º del grado en ${content[1]} no existe. Contacte con un administrador para resolver la incidencia.`)
+							break
+						case 4:
+							client.sendMessage(message.from, `El grado en ${content[1]} no existe. Contacte con un administrador para resolver la incidencia.`)
+							break
+					}
+					chat.clearState()
+					break
+
+				case 'geteventlist':
+					chat.sendStateTyping()
+					logger('Event list requested.', message)
+					sendEventList(message)
 					chat.clearState()
 					break
 
 				default:
-					break
+					logger(`Plain message from ${message.from}.`)
 			}
 		}
 	})
-
 	client.initialize()
 }
 
 
-/**Function that sends a formatted event list to the message sender's chat.*/
+/**Sends a formatted event list to the message sender's chat.*/
 async function sendEventList(message) {
-	let events = require(formatter.DATABASE)
-	let identifiers = Object.keys(events)
+	let user = require(users.USER_DATABASE)[message.from]
 
-	let rows = []
-	let sections = [{title: 'Eventos:', rows: rows}]
+	Object.keys(user).forEach(degree =>
+		user[degree].forEach(function (year) {
+			let events = require(event_fetcher.EVENTS)[degree][year]
+			let identifiers = Object.keys(events)
 
-	identifiers.forEach(function (identifier) {
-		sections[0].rows.push({
-			id: identifier,
-			title: events[identifier].name,
-			description: events[identifier].description != '' ? events[identifier].description.length <= 39 ? events[identifier].description : events[identifier].description.slice(0, 39) + '...' : 'No existe descripción para este evento.'
+			let rows = []
+			let sections = [{title: 'Eventos:', rows: rows}]
+
+			identifiers.forEach(function (identifier) {
+				sections[0].rows.push({
+					id: identifier,
+					title: events[identifier].name,
+					description: events[identifier].description != '' ? events[identifier].description.length <= 39 ? events[identifier].description : events[identifier].description.slice(0, 39) + '...' : 'No existe descripción para este evento.'
+				})
+			})
+
+			client.sendMessage(message.from, new List(`Lista de eventos disponibles para ${year}º del grado en ${degree}.\n\n_Pulsa el botón de la parte inferior para continuar._`, 'Ver eventos', sections, '*Próximos eventos:*', ''))
 		})
-	})
-
-	client.sendMessage(message.to, new List('_Pulsa el botón de la parte inferior para ver todos los eventos disponibles._', 'Ver eventos', sections, '*Próximos eventos:*', ''))
+	)
 }
 
 
-/**Function that serves as handler for list-specific replies.*/
+/**Handler for list-specific replies.*/
 async function listReplyHandler(message) {
-	let event = require(formatter.DATABASE)[message.selectedRowId]
+	let user = require(users.USER_DATABASE)[message.from]
+	let events = require(event_fetcher.EVENTS)
+	let event;
+
+	Object.keys(user).forEach(degree =>
+		user[degree].forEach(function (year) {
+			if (Object.keys(events[degree][year]).includes(message.selectedRowId)) {
+				event = events[degree][year][message.selectedRowId]
+			}
+		})
+	)
+
 	let button = new Buttons(
-		`\n${formatter.eventStringify(event)}`,
+		`\n${event_fetcher.eventStringify(event)}`,
 		[{id: `${event.date}`, body: 'Ver tiempo restante'}],
 		`Evento: ${event.name.toUpperCase()}`,
 		`\nVisita ${event.url} para más información acerca del evento.`
 	)
 
-	client.sendMessage(message.to, button)
+	client.sendMessage(message.from, button)
 }
 
 
-/**Function that serves as handler for button-specific replies.*/
+/**Handler for button-specific replies.*/
 async function buttonReplyHandler(message) {
-	let difference = formatter.dateDifference(new Date(Date.now()), new Date(parseInt(message.selectedButtonId)))
-	client.sendMessage(message.to, `Tiempo restante: ${difference.days} día(s), ${difference.hours} hora(s), ${difference.minutes} minuto(s), ${difference.seconds} segundo(s).`)
+	let difference = event_fetcher.dateDifference(new Date(Date.now()), new Date(parseInt(message.selectedButtonId)))
+	client.sendMessage(message.from, `Tiempo restante: ${difference.days} día(s), ${difference.hours} hora(s), ${difference.minutes} minuto(s), ${difference.seconds} segundo(s).`)
 }
 
 
--- users.js
+++ users.js
@@ -1,26 +1,112 @@
+// Remote modules:
 const fs = require('fs')
 
-const DATABASE = './users.json'
+// Constants and variables:
+const DEGREE_DATABASE = './subscriptions.json'
+const USER_DATABASE = './users.json'
 
-function addUser(phone, degree, level) {
-	let db = require(DATABASE)
 
-	db[phone] = {
-		degree: degree,
-		level: level
+/*Adds a phone to a specific year and degree.**/
+function addUser(phone, degree, year) {
+	let degreedb = require(DEGREE_DATABASE)
+	let userdb = require(USER_DATABASE)
+
+	if (!Object.keys(userdb).includes(phone)) {
+		userdb[phone] = {}
 	}
 
-	updateUsers(db)
+	if (Object.keys(degreedb).includes(degree)) {
+		if (Object.keys(degreedb[degree]).includes(year)) {
+			if (Object.keys(userdb[phone]).includes(degree)) {
+				if (!userdb[phone][degree].includes(year)) {
+					userdb[phone][degree].push(year)
+					degreedb[degree][year].push(phone)
+
+					updateDatabase(DEGREE_DATABASE, degreedb)
+					updateDatabase(USER_DATABASE, userdb)
+					return 0
+				}
+				return 3  // Year already registered.
+			}
+			userdb[phone][degree] = [year]
+			degreedb[degree][year].push(phone)
+
+			updateDatabase(DEGREE_DATABASE, degreedb)
+			updateDatabase(USER_DATABASE, userdb)
+			return 0
+		}
+		return 2  // Invalid year.
+	}
+	return 1  // Invalid degree.
 }
 
-function removeUser(phone) {
-	let db = require(DATABASE)
-	delete db[phone]
-	updateUsers(db)
+
+/*Adds a new degree.**/
+function addDegree(degree) {
+	let degreedb = require(DEGREE_DATABASE)
+
+	if (Object.keys(degreedb).includes(degree)) {
+		return 1
+	}
+
+	degreedb[degree] = {
+		"1": [],
+		"2": [],
+		"3": [],
+		"4": []
+	}
+
+	updateDatabase(DEGREE_DATABASE, degreedb)
+
+	return 0
 }
 
-async function updateUsers(db) {
-	fs.writeFile(DATABASE, JSON.stringify(db),
+
+/*Removes a phone from a specific year and degree.**/
+function removeUser(phone, degree, year) {
+	let degreedb = require(DEGREE_DATABASE)
+	let userdb = require(USER_DATABASE)
+
+	if (Object.keys(degreedb).includes(degree)) {
+		if (Object.keys(degreedb[degree]).includes(year)) {
+			if (Object.keys(userdb[phone]).includes(degree)) {
+				if (userdb[phone][degree].includes(year)) {
+					if (userdb[phone][degree].length == 1) {
+						delete userdb[phone][degree]
+					} else {
+						userdb[phone][degree].splice(userdb[phone][degree].indexOf(year), 1)
+					}
+
+					degreedb[degree][year].splice(degreedb[degree][year].indexOf(phone), 1)
+
+					updateDatabase(DEGREE_DATABASE, degreedb)
+					updateDatabase(USER_DATABASE, userdb)
+
+					return 0
+				}
+				return 1  // Year not registered.
+			}
+			return 2  // Degree not registered.
+		}
+		return 3  // Invalid year.
+	}
+	return 4  // Invalid degree.
+}
+
+
+/*Updates the specified database with a JSON object.**/
+async function updateDatabase(database_file, database) {
+	fs.writeFile(database_file, JSON.stringify(database),
 		err => err ? console.error(err) : null
 	)
+}
+
+
+module.exports = {
+	DEGREE_DATABASE,
+	USER_DATABASE,
+
+	addUser,
+	addDegree,
+	removeUser
 }
